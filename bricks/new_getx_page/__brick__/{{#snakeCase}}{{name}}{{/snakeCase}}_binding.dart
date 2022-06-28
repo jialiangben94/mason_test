8b72d3133d43f7dea691cf483996399163cc0d4b
Benjamin <benjaminetc@etctech.com.my>
2022-06-28 13:06:39 +0800

    Commit changes

--- bricks/new_getx_page/__brick__/{{#snakeCase}}{{name}}{{/snakeCase}}_binding.dart
+++ bricks/new_getx_page/__brick__/{{#snakeCase}}{{name}}{{/snakeCase}}_binding.dart
@@ -1,4 +1,3 @@
-import 'package:{{{project_name}}}/app/ui/cart/my_cart/my_cart_controller.dart';
 import 'package:get/get.dart';
 
 class {{#pascalCase}}{{name}}{{/pascalCase}}Binding extends Bindings {
