9297592b764da45646f043fc7007a6c97dd6e095
Benjamin <benjaminetc@etctech.com.my>
2022-06-29 14:54:56 +0800

    Commit changes

--- bricks/new_getx_page/__brick__/{{name.snakeCase()}}_page.dart
+++ bricks/new_getx_page/__brick__/{{name.snakeCase()}}_page.dart
@@ -3,7 +3,6 @@ import 'package:get/get.dart';
 import 'package:{{{project_name}}}/app/ui/base/base_page.dart';
 
 class {{#pascalCase}}{{name}}{{/pascalCase}}Page extends BasePage<{{#pascalCase}}{{name}}{{/pascalCase}}Controller>  {
-  const {{#pascalCase}}{{name}}{{/pascalCase}}Page({Key key}) : super(key: key);
 
   @override
   // TODO: implement routeName
